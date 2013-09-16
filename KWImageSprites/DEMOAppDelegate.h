aa5cb5cebbc8dc7aa54e2982cce2a080b1aafeab
kawanet <u-suke@kawa.net>
2013-09-16 20:43:30 +0900

    Initial Commit

---
+++ KWImageSprites/DEMOAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DEMOAppDelegate.h
+//  KWImageSprites
+//
+//  Created by Yusuke Kawasaki on 2013/09/16.
+//  Copyright (c) 2013年 Kawanet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DEMOAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ KWImageSprites/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  KWImageSprites
+//
+//  Created by Yusuke Kawasaki on 2013/09/16.
+//  Copyright (c) 2013年 Kawanet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DEMOAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DEMOAppDelegate class]));
+    }
+}
